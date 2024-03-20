bab3fb87bf12cdb1105331587ad93ba95a866410
laura <kellukesed3@gmail.com>
2024-03-20 12:53:39 +0200

    Add project basic file

--- game.py
+++ game.py
@@ -1,25 +1,18 @@
 import pygame
-from settings import Settings
-from player import Player
-import game_functions as gf
-def run_game():
-    pygame.init()
-    gm_settings = Settings()
-    
-    # Set up the drawing window
-    screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
-    pygame.display.set_caption(gm_settings.caption)
+pygame.init()
 
-    # Instantiate player
-    player = Player(screen)
-    
-    # Run until the user asks to quit
-    while True:
-        screen.fill(gm_settings.bg_color)
-        
-        gf.check_events()
-        player.update()
-        gf.update_screen(gm_settings, screen, player)
-        # Draw the player on the screen
 
-run_game()
+screen = pygame.display.set_mode([800, 500])
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    
+    pygame.display.flip()
+    
+    
+pygame.quit()
